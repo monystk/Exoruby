18c8a0192aeb450dbb61e84c96a983a4a4bb1c24
Dakota <98390107+monystk@users.noreply.github.com>
2022-04-16 19:08:33 +0200

    Update exo-10.rb

--- exo-10.rb
+++ exo-10.rb
@@ -1,1 +1,12 @@
-#Afficher tous les âges
+#Afficher tous les âges + date de naissance
+
+puts "Annee de naissance ?"
+date_birtdate = gets.chomp.to_i
+age = 1
+
+puts "Annee\tAge"
+while date_birtdate <= 2022
+    puts "#{date_birtdate}\t#{age}"
+    date_birtdate += 1
+    age += 1
+end
