19a51efe782f0200046c14467f32fa6192f84262
Dakota <98390107+monystk@users.noreply.github.com>
2022-04-16 20:57:36 +0200

    Update exo-17.rb

--- exo-17.rb
+++ exo-17.rb
@@ -1,5 +1,6 @@
 #La pyramide, version expert
 # demander à l'utilisateur un nombre entre 1 et 25 et qui va sortir une pyramide qui monte et qui descend
+#1ere façon de faire
 
 puts " choisis un chiffre entre 1 et 25 "
 n= gets.chomp. to_i
@@ -11,4 +12,28 @@ while i <=n-1
    puts("#"*1).chomp ("#")
 end
 
+    
+    
+    
+    
+    
+#2eme façon de faire 
 
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+user_number = gets.chomp.to_i
+while user_number < 1 || user_number > 25
+    puts "ERREUR"
+    user_number = gets.chomp.to_i
+end
+    
+puts "Voici la pyramide"
+    
+block_number=1
+spaces_number = user_number - 1
+for i in 1..user_number do
+    print " " * spaces_number
+    puts "#" * block_number
+    block_number += 2
+    spaces_number -= 1
+    
+end
