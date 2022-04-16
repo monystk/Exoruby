5e98ab209832be696b0b3efdd48fc4e7e5b4a1fe
Dakota <98390107+monystk@users.noreply.github.com>
2022-04-16 19:35:59 +0200

    Update exo-11.rb

--- exo-11.rb
+++ exo-11.rb
@@ -1,3 +1,25 @@
 #Virer les années qui va demander son âge à l'utilisateur, et qui,
 # pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".()
 
+
+
+puts "Age ?"
+age = gets.chomp.to_i
+date_birthdate = 2022 - age
+age = 0
+
+while date_birthdate <= 2022
+
+    if age == 0
+        puts "il y a #{2022 - date_birthdate} ans, tu n'etais pas née"
+    
+    elsif 2022 - date_birthdate == 0
+        puts "De nos jour, tu as #{age} ans"
+    
+    else
+        puts "il y a #{2022 - date_birthdate} ans, tu avais #{age} ans"
+    end
+    
+    date_birthdate += 1
+    age += 1
+end
